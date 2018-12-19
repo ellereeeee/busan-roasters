e92a37f035308bbf51a281c882415eb85f66a74d
Richard Ellery <raellery@gmail.com>
2018-12-19 21:14:31 +0900

    Toggle slide animation on drop down menu

--- script.js
+++ script.js
@@ -2,5 +2,7 @@ $(document).ready(function(){
   $('#nav-icon').click(function() {
     // toggle hamburger/X icon on click
     $(this).toggleClass('open');
+    // toggle slide animation on drop down menu
+    $('#drop-down-menu').slideToggle(250);
   });
 });
